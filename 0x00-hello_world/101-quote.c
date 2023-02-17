f3b8df43d21f5765b18a0c939f0cf54ef2c51121
swambuis <wambuis21@gmail.com>
2023-02-17 02:38:05 -0800

    print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-include < unistd. >
+#include <unistd.h>
+
 /**
  * main - c program that prints a line to the standard error
- * Return: Always 0 (Success)
+ *
+ * Return: Always 1 (Success)
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19));
+	write(2, 
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
