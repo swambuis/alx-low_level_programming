ecfa2a6710397be6cc0972707dffa02785ef7d3a
swambuis <wambuis21@gmail.com>
2023-02-17 17:46:46 -0800

    Aplhabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
 */
@@ -10,7 +10,7 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while 
+	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
