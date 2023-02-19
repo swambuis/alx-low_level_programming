68dc648ee18d065d0d5e22731923e42b6d7b84a1
swambuis <wambuis21@gmail.com>
2023-02-19 11:37:38 -0800

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - printing lowercase alphabets in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
