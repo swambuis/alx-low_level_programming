b41c6bf441e5d1b2e733d5fb2621dfe38f1efaff
swambuis <wambuis21@gmail.com>
2023-02-19 10:36:18 -0800

    positive, zero, negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
 	else
-		printf("%d is negative\n", n);
+		printf("%i is zero\n", n);
 	return (0);
 }
