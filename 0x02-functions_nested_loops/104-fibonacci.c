6e852302694f89f2e31db945b8ea9d9b1393f7fd
swambuis <wambuis21@gmail.com>
2023-02-22 09:56:47 -0800

    print first 98 fib numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 fibonacci numbers, starting with 1 and 2, separated by a comma followed by space.
+ * main - prints the first 98 fibonacci numbers, starting with 1 and 2
  * Return: Always 0.
  */
 
