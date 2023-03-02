52a38ab2961d2c7b1129377fac93b1ca3361333e
swambuis <wambuis21@gmail.com>
2023-03-01 22:30:16 -0800

    change lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
-
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
  * Return: n
  */
-
 char *string_toupper(char *n)
 {
 int i;
@@ -19,4 +17,3 @@ i++;
 }
 return (n);
 }
-
