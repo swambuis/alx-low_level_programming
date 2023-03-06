4d31cd2573234e7e66fcc8724d813f22aaf9b1b5
swambuis <wambuis21@gmail.com>
2023-03-06 11:55:28 -0800

    locate character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - Locate character string
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+for (; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
+}
