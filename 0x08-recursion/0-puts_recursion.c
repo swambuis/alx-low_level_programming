09e956bf1a7c6aadb729f0a4e207371040720464
swambuis <wambuis21@gmail.com>
2023-03-08 13:04:06 -0800

    print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
+ * _puts_recursion - function that prints a string, followed by a new line
  * @s: input
  * Return: Always 0 (Success)
  */
