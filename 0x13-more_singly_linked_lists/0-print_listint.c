cb5e1fa9f0a295382b61b4869e8fb99add71b090
swambuis <wambuis21@gmail.com>
2023-04-04 02:15:55 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t no = 0;
-while (h)
+int i = 0;
+while (h != NULL)
 {
-no++;
-printf("%d\n", h->n);
+printf("%i\n", h->n);
 h = h->next;
+i++;
 }
-return (no);
+return (i);
 }
